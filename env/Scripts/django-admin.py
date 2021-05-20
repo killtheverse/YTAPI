63e0555d2482940dd8945e905dcd8023f64e509c
killtheverse <r.dev2000@gmail.com>
2021-05-20 12:55:10 +0530

    Fixed .gitignore

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\hp\desktop\innovaccer\tasks\task1-youtubeapi\ytapi\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

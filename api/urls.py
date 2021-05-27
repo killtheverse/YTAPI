56062c49cabf8e1564d9d6ec3cfe36f64b3cc102
Rahul <r.dev2000@gmail.com>
2021-05-27 13:52:09 +0530

    Removed dead urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import (
     bulk_fetch, bulk_register, query_list, query_detail
-    # register_query, get_query_results,
-    # get_user_queries, delete_query
 )
 
 urlpatterns = [
@@ -10,8 +8,4 @@ urlpatterns = [
     path('queries/<str:slug>/', query_detail),
     path('bulk_fetch/', bulk_fetch),
     path('bulk_register/', bulk_register),
-    # path('queries/', register_query),
-    # path('get_query_results/', get_query_results),  
-    # path('get_user_queries/', get_user_queries),
-    # path('delete_query/', delete_query),
 ]
